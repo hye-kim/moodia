a3a1155c3d956c004c30cc701df1adba04944825
hye <davidckim22@gmail.com>
2021-06-14 03:54:13 -0400

    updating footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -2,15 +2,20 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex items-center py-2 bg-white">
+    <div className="flex items-center py-2" style={{backgroundColor: "#666666"}}>
       <div className="pl-16">
         <a href="https://www.linkedin.com/in/hye-kim/">
-          <i className="fab fa-linkedin-in text-gray-700 text-4xl"></i>
+          <i className="fab fa-linkedin-in text-white text-4xl"></i>
         </a>
       </div>
       <div className="pl-12">
         <a href="https://github.com/hye-kim/moodia">
-          <i className="fab fa-github text-gray-700 text-4xl"></i>
+          <i className="fab fa-github text-white text-4xl"></i>
+        </a>
+      </div>
+      <div className="pl-12">
+        <a href="https://hyekim.me/">
+          <i className="fas fa-user text-white text-4xl"></i>
         </a>
       </div>
     </div>
