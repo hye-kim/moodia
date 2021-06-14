845d5d50d6f356ec049185e6175343edea309575
hye <davidckim22@gmail.com>
2021-06-14 03:47:36 -0400

    updated landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ import LandingCard from "./LandingCard";
 import LandingNavBar from "./LandingNavBar";
 import moodImg from "../../images/landing-mood.png";
 import habitImg from "../../images/landing-habits.png";
+import goalImg from "../../images/landing-goals.png";
 import Footer from "../Footer/Footer";
 
 function LandingPage({ user }) {
@@ -32,11 +33,22 @@ function LandingPage({ user }) {
         <LandingCard
           title="Plan Your Day"
           reverse={true}
-          description="It's important to build good habits. Organize your morning and evening routines with Moodia."
+          description="It's important to build good habits. Organize and stay on top of your day."
           child={
             <img
               src={habitImg}
               alt="habits"
+              className="max-w-xs md:max-w-xl"
+            ></img>
+          }
+        ></LandingCard>
+        <LandingCard
+          title="Set Goals"
+          description="Be goal-oriented! Keep up to date with the progress on your goals."
+          child={
+            <img
+              src={goalImg}
+              alt="goals"
               className="max-w-xs md:max-w-xl"
             ></img>
           }
